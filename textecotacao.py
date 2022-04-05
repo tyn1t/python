24acffeb23b7959edccf8a81072112beaca8b794
giovanni <92416664+tyn1t@users.noreply.github.com>
2022-04-05 14:21:19 -0400

    testecotacao.py
    teste cotação

---
+++ textecotacao.py
@@ -0,0 +1,16 @@
+from Qualmelhorcambio import Melhorcambio
+
+Q = Melhorcambio(41,9.9,200)
+Q.peso_receber()
+Q.taxa_peso()
+Q.valor_total()
+Q.resumo()
+print("+=========+========+")
+Q = Melhorcambio(38,0,200)
+Q.peso_receber()
+Q.taxa_peso()
+Q.valor_total()
+Q.taxa_dois(9.9)
+Q.resumo()
+
+ 
