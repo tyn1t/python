1ceb0090d638ec4757176d602060dcdd4604cba4
tyn1t <92416664+tyn1t@users.noreply.github.com>
2022-04-05 00:58:50 -0400

    Add files via upload
    Ver qual melhor cotação do peso,primeiro projeto sem cola kkk.

---
+++ Qualmelhorcambio.py
@@ -0,0 +1,44 @@
+class Melhercambio:
+    def __init__(self,peso_cotacao,taxa,real =0):
+        self.real = real
+        self.taxa = taxa
+        self.peso_cotacao = peso_cotacao
+        self.operacao = []
+        
+        
+    def peso_receber(self):
+        peso_receber = self.real * self.peso_cotacao
+        self.operacao.append(["Valo Receber em peso",peso_receber])
+        self.valor_apaga = peso_receber
+    def taxa_peso(self): 
+        taxa_peso = self.taxa * self.peso_cotacao
+        self.operacao.append(["Valor da taxa em peso:",taxa_peso])
+        self.taxa_apaga = taxa_peso
+
+    def valor_total(self): 
+        total = self.taxa_apaga + self.valor_apaga
+        self.operacao.append(["Total a paga",total])
+
+
+    def taxa_dois(self,taxa_soma):
+        while True:
+            try:
+                perguta = input("Soma Taxa do primeiro valor como segundo para saber total [s] uo [n]:")
+                if perguta.upper() == "S":
+                    s = self.valor_apaga + (taxa_soma * self.peso_cotacao) 
+                    return self.operacao.append(["Total + Taxa",s])
+                elif perguta.upper() == "N":
+                    print('pense bem')
+                    break
+            except Exception:
+                print("Algo de errado aconteceu")
+
+                  
+    def resumo(self):
+        print(f"Teste programa de cotação ")
+        print(f'Valor para enviar:R${self.real:6.2f}')
+        for o in self.operacao:
+            print(f"{o[0]} {o[1]}  ")
+
+  
+               
